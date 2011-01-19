4ae9c8929421e61cba606bacef77fa6f26d6840c
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2011-01-19 15:19:53 +0100

    download middleware fixed

--- lib/thumbs/middleware/download.rb
+++ lib/thumbs/middleware/download.rb
@@ -10,18 +10,18 @@ module Thumbs
     def call(env)
       if env['thumbs.original_path']
         begin
-          [200, {}, File.open(env['thumbs.original_path']).read]
-        rescue Errno::ENOENT, IOError
+          return [200, {}, File.open(env['thumbs.original_path']).read]
+        rescue Errno::ENOENT, IOError => e
         end
       end
       if env['thumbs.remote_url']
         begin
-          [200, {}, open(env['thumbs.remote_url']).read]
+          return [200, {}, open(env['thumbs.remote_url']).read]
         rescue StandardError, Timeout::Error => e
-          [404, {'Content-Type' => 'text/plain'}, ["Not found"]]
+          return [404, {'Content-Type' => 'text/plain'}, ["Not found"]]
         end
       else
-        @app.call(env)
+        return @app.call(env)
       end
     end
     
