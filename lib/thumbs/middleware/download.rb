ded10ca3fad1c39f4c96a7cd0526bc5ae9de3598
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2011-01-19 12:39:07 +0100

    download fix

--- lib/thumbs/middleware/download.rb
+++ lib/thumbs/middleware/download.rb
@@ -13,16 +13,15 @@ module Thumbs
           [200, {}, File.open(env['thumbs.original_path']).read]
         rescue Errno::ENOENT, IOError
         end
-      else
-        if env['thumbs.remote_url']
-          begin
-            [200, {}, open(env['thumbs.remote_url']).read]
-          rescue StandardError, Timeout::Error => e
-            [404, {'Content-Type' => 'text/plain'}, ["Not found"]]
-          end
-        else
-          @app.call(env)
+      end
+      if env['thumbs.remote_url']
+        begin
+          [200, {}, open(env['thumbs.remote_url']).read]
+        rescue StandardError, Timeout::Error => e
+          [404, {'Content-Type' => 'text/plain'}, ["Not found"]]
         end
+      else
+        @app.call(env)
       end
     end
     
