aa0cdbd4e1aac35f43d5d223782297f33adaa18c
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2011-03-30 10:38:26 +0200

    ruby 1.9.2 compatibility

--- lib/thumbs/middleware/cache_read.rb
+++ lib/thumbs/middleware/cache_read.rb
@@ -9,7 +9,7 @@ module Thumbs
     def call(env)
       if env["thumbs.#{@cache_type}_path"]
         begin
-          status, headers, body = [200, {}, File.read(env["thumbs.#{@cache_type}_path"])]
+          status, headers, body = [200, {}, [File.read(env["thumbs.#{@cache_type}_path"])]]
           env['thumbs.logger'] << "#{@cache_type}_cache" if env['thumbs.logger']
           return [status, headers, body]
         rescue Errno::ENOENT, IOError => e
