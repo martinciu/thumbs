e95398b7ca42e029c94cfd5a654623761485a3a7
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2011-04-04 22:15:02 +0200

    goliath example app

---
+++ examples/app.rb
@@ -0,0 +1,25 @@
+$: << File.expand_path("../../lib", __FILE__)
+require "rubygems"
+require "thumbs"
+require "goliath"
+
+thumbs = Thumbs.new(
+  :url_map         => "/:size/:original_url", # url pattern, :original_url is required
+  :thumbs_folder   => "cache", # default: false - do not write on disk
+  :cache           => true,                   # cache resized images - has no effect unless thumbs_folder exist
+  :cache_original  => true,                   # cache original images - has no effect unless thumbs_folder exist
+  :runtime         => true,                   # add X-Runtime header
+  :cache_control => {
+    :ttl           => 86400,                  # add Expires and Cache-Control header for 24h
+    :last_modified => true                    # add Last-Modfied header
+  },
+  :etag            => true,                   # add ETag
+  :logfile         => false,                  # will write log file if filepath is provided
+  :server_name     => "Thumbs/0.0.8",         # Server header
+  :image_not_found => "image_not_found.jpg"   # path to image_not_found
+)
+
+runner = Goliath::Runner.new(ARGV, nil)
+runner.app = thumbs
+runner.run
+
